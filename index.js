3095138e2968905ae52cf402c298ce8ca63643eb
Tommy Svensson <tommykajsvensson@gmail.com>
2019-05-08 08:32:30 +0200

    finalize chat

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/:message', (req, res) => res.send(req.params.message));
+app.get('/:message', (req, res) => {
+  fs.appendFile('chat.txt', req.params.message, () => {
+    return fs.readFile('chat.txt', 'utf8', (err, data) => {
+      if(err) throw err;
+      res.send(data);
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
